366ebfec887f7c0de251c24e19579d1a5f76f75d
Prakul Bansal <65239339+prakul1982@users.noreply.github.com>
2020-11-10 20:09:27 +0530

    Add files via upload

---
+++ screens/BookTransctionScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class BookTransctionScreen extends  React.Component{
+    render(){
+        return (
+            <View>       
+                <Text>     
+                    issue
+                </Text> 
+                   
+                </View>
+
+        )
+    }
+}
